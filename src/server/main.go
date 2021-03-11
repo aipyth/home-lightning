98a44366fb67011cbdca51bb3cf572b7cb4fdfc4
Ivan <aipyth.func@gmail.com>
2021-03-11 18:24:43 +0200

    server side done

---
+++ src/server/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+//  ________________________
+// |------------------------|
+// |                       -|
+// |                       -|___________________________________
+// |                       ------------------------------------ |
+// |                                                            |
+// |                                                            |
+// |                                                            |
+// |                                                            |
+// |------------------------                                    |
+// |_______________________-                                   -|
+// |-                     |-                                   -|
+// |-                     |-                                   -|
+// |-                     |-                                   -|
+// |-                     |                                    -|
+// |-                     |                                    -|
+// |-                     |                      -------------- |
+// |____________________________________________________________|
+
+type LEDLine struct {
+	Mode       string
+	Place      string
+	Color      string
+	Brightness float32
+}
+
+var Modes = []string{
+	"oneline",
+	"off",
+}
+
+func StartBot(leds []LEDLine) {
+	b, err := tb.NewBot(tb.Settings{
+		Token:  "1467814308:AAES_W2E8Nudwd_tcHk2nwjAJzVERWctZUM",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/leds", func(m *tb.Message) {
+
+		ledsKeyboard := make([][]tb.InlineButton, 0)
+
+		for _, l := range leds {
+			ledsKeyboard = append(ledsKeyboard, []tb.InlineButton{
+				tb.InlineButton{
+					Unique: l.Place,
+					Text:   "Line " + l.Place,
+				},
+			})
+		}
+
+		b.Send(m.Sender, "Hello World!", &tb.ReplyMarkup{
+			InlineKeyboard: ledsKeyboard,
+		})
+	})
+
+	b.Handle(tb.OnCallback, func(c *tb.Callback) {
+		//ledActions := []string{
+		//	"brightness",
+		//	"mode",
+		//}
+		//
+		//action := "none"
+		//for _, l := range leds {
+		//	if l.Place == c.Data {
+		//
+		//	}
+		//}
+		//
+		//
+		//if action == "place" {
+		//	b.Send(c.Sender, "What to do?", &tb.ReplyMarkup{
+		//
+		//	})
+		//} else if action == "led" {
+		//
+		//}
+
+
+		b.Respond(c)
+	})
+
+	b.Start()
+}
+
+func StartServer(leds []LEDLine) {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+
+	r.GET("/modes", func(c *gin.Context) {
+		c.JSON(200, Modes)
+	})
+
+	r.GET("/leds", func(c *gin.Context) {
+		c.JSON(200, leds)
+	})
+	r.POST("/leds", func(c *gin.Context) {
+		jsonData, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			log.Println("ERROR", err)
+		}
+		err = json.Unmarshal(jsonData, &leds)
+		if err != nil {
+			log.Println("ERROR", err)
+		}
+	})
+
+	r.GET("/", func (c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"leds": leds,
+		})
+	})
+	r.Run()
+}
+
+func main() {
+	LEDs := []LEDLine{
+		LEDLine{
+			Mode:       "oneline",
+			Place:      "ceil",
+			Color:      "#ffffff",
+			Brightness: float32(0),
+		},
+		LEDLine{
+			Mode:       "off",
+			Place:      "kitchen",
+			Color:      "#ffffff",
+			Brightness: float32(0),
+		},
+	}
+
+	go StartBot(LEDs)
+	StartServer(LEDs)
+}
---
+++ src/server/static/js/index.js
@@ -0,0 +1,120 @@
+let LEDS = []
+let Modes = []
+
+async function getLeds() {
+    let resp = await fetch("/leds")
+    if (resp.ok) {
+        LEDS = await resp.json()
+        console.info("INFO", LEDS)
+    } else {
+        console.error("ERROR", resp.status)
+    }
+}
+
+async function getModes() {
+    let resp = await fetch("/modes")
+    if (resp.ok) {
+        Modes = await resp.json()
+        console.info("INFO", Modes)
+    } else {
+        console.error("ERROR", resp.status)
+    }
+}
+
+async function sendLeds() {
+    let resp = await fetch("/leds", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(LEDS)
+    })
+}
+
+function changeBrightness(place) {
+    let selector = `#${place.id} input[type='range']`
+    let br = document.querySelector(selector).value
+    for (i = 0; i < LEDS.length; i++) {
+        if (LEDS[i].Place == place.id) {
+            LEDS[i].Brightness = br / 100
+        }
+    }
+    sendLeds()
+}
+
+function changeColor(place) {
+    let selector = `#${place.id} input[type='color']`
+    let color = document.querySelector(selector).value
+    for (i = 0; i < LEDS.length; i++) {
+        if (LEDS[i].Place == place.id) {
+            LEDS[i].Color = color
+        }
+    }
+    sendLeds()
+}
+
+function changeMode(place) {
+    let selector = `#${place.id} select`
+    let mode = document.querySelector(selector).value
+    for (i = 0; i < LEDS.length; i++) {
+        if (LEDS[i].Place == place.id) {
+            LEDS[i].Mode = mode
+        }
+    }
+    sendLeds()
+}
+
+function renderLEDSInterface() {
+
+    let out = ""
+    for (i = 0; i < LEDS.length; i++) {
+        l = LEDS[i]
+
+        s = `
+        <div class="led" id="${l.Place}">
+            <h1>${l.Place}</h1>
+            <div class="form-group">
+        `
+
+        // render modes
+        s += `<select class="form-control form-control-lg" oninput="changeMode(${l.Place})">`
+        for (j = 0; j < Modes.length; j++) {
+            if (LEDS[i].Mode == Modes[j]) {
+                // current active mode
+                s += `
+                <option selected value="${Modes[j]}">${Modes[j]}</option>
+                `
+            } else {
+                s += `
+                <option value="${Modes[j]}">${Modes[j]}</option>
+                `
+            }
+        }
+        s += `</select>`
+
+        // brightness and color
+        s += `
+            <p>
+            <label>Brightness
+            <input type="range" class="form-control-range"
+                oninput="changeBrightness(${l.Place});" value="${l.Brightness * 100}">
+            </label>
+            </p>
+            <p>
+            <label>Color
+            <input type="color" value="${l.Color}"
+                oninput="changeColor(${l.Place})">
+            </label>
+            </p>
+        `
+        // end rendering string
+        s += `</div></div>`
+
+        out += s
+    }
+    document.querySelector("#leds").innerHTML = out
+}
+
+getModes().then( () => {
+    getLeds().then(renderLEDSInterface)
+})
